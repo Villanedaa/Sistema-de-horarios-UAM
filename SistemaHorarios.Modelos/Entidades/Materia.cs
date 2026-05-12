6d6708ad79c7eb4fa0441fd37bff99dc2b158a1a
Natalia Velez Orjuela <natalia.velez.orjuela@gmail.com>
2026-05-12 01:39:51 -0500

    chore: ignorar archivos generados

---
+++ SistemaHorarios.Modelos/Entidades/Materia.cs
@@ -0,0 +1,29 @@
+﻿namespace SistemaHorarios.Modelos.Entidades;
+
+public class Materia
+{
+    public int IdMateria { get; set; }
+
+    public string Codigo { get; set; } = string.Empty;
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public int Creditos { get; set; }
+
+    public int IntensidadHorariaSemanal { get; set; }
+
+    public int SemestreSugerido { get; set; }
+
+    public bool Activa { get; set; } = true;
+
+    public Materia()
+    {
+        IdMateria = 0;
+        Codigo = string.Empty;
+        Nombre = string.Empty;
+        Creditos = 0;
+        IntensidadHorariaSemanal = 0;
+        SemestreSugerido = 0;
+        Activa = true;
+    }
+}
