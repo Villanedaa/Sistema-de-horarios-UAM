f1f21b1e527687e52e92dabc68ebcab6bcf09545
Luis Carlos <luiscarlosgallegomorales94@gmail.com>
2026-05-12 23:36:16 -0500

    feat: Se crea la carpeta negocio con la base inicial de la logica

---
+++ SistemaHorarios.Logica/Negocio/Materias/Materias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHorarios.Logica.Negocio.Materias
+{
+    internal class Materias
+    {
+        // Aquí puedes implementar la lógica relacionada con las materias, como agregar, eliminar, actualizar información, etc.
+    }
+}
