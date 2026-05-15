452cb9cf896977b39fd0a06d386790bcc0f14fb1
Luis Carlos <luiscarlosgallegomorales94@gmail.com>
2026-05-15 02:46:27 -0500

    refactor: quitar referencias innecesaria en materia

--- SistemaHorarios.Modelos/Entidades/Materia.cs
+++ SistemaHorarios.Modelos/Entidades/Materia.cs
@@ -2,16 +2,21 @@
 
 namespace SistemaHorarios.Modelos.Entidades;
 
+// Representa una materia registrada dentro del sistema.
 public class Materia
-
-    
 {
     [Key]
     public int IdMateria { get; set; }
+
     public string Codigo { get; set; } = string.Empty;
+
     public string Nombre { get; set; } = string.Empty;
+
     public int Creditos { get; set; }
+
     public int IntensidadHorariaSemanal { get; set; }
+
     public int Semestre { get; set; }
+
     public bool Activa { get; set; } = true;
 }
